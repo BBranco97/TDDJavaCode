51366e3772b691e1015853cc5c331697ed91b00e
BBranco97 <131369036+BBranco97@users.noreply.github.com>
2024-04-10 16:17:17 -0300

    feat: Get Temperature Method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,31 @@ package org.example;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
 
-}
+public class Main {
+
+    public static void main(String[] args) {
+        int[] temp = new int[7];
+        temp = GetTemperature();
+//        double avg = AVGWeekTemperature(temp);
+//        int aboveAvg = DaysAboveAVG(temp, avg);
+
+//        System.out.println("Dias com temperatura acima da média: " + aboveAvg);
+    }
+
+    public static int[] GetTemperature() {
+        int[] temp = new int[7];
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite as temperaturas da semana separadas por espaço:");
+        for (int i = 0; i < 7; i++) {
+            if (scanner.hasNextInt()) {
+                temp[i] = scanner.nextInt();
+            } else {
+                System.out.println("Erro");
+                throw new IllegalArgumentException("O array contém um elemento que não é do tipo inteiro.");
+            }
+        }
+
+        return temp;
+    }
