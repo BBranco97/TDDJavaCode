e3989d32d4e2b80db1fe19c908623fe772373987
BBranco97 <131369036+BBranco97@users.noreply.github.com>
2024-04-10 16:28:02 -0300

    feat: Calculate AVG week temperature

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         int[] temp = new int[7];
         temp = GetTemperature();
-//        double avg = AVGWeekTemperature(temp);
+        double avg = AVGWeekTemperature(temp);
 //        int aboveAvg = DaysAboveAVG(temp, avg);
 
 //        System.out.println("Dias com temperatura acima da média: " + aboveAvg);
@@ -30,3 +30,17 @@ public class Main {
 
         return temp;
     }
+    public static double AVGWeekTemperature(int[] temp) {
+
+        if (temp.length != 7) {
+            System.out.println("Erro");
+            throw new IllegalArgumentException("Não foram fornecidas 7 temperaturas");
+        }
+
+        int sum = 0;
+        for (int t : temp) {
+            sum += t;
+        }
+        return sum / 7.0;
+    }
+}
